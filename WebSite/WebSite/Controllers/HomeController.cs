4d8f09eb8844452d8dda467eb7cee42d0d9147ea
Netrapida <Daeynasvistas@gmail.com>
2017-10-13 14:57:31 +0100

    Alteração do controler da página de About "Message" alterado

--- WebSite/WebSite/Controllers/HomeController.cs
+++ WebSite/WebSite/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace WebSite.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Montes de cenas porreiras.";
 
             return View();
         }
